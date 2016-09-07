32554eeb45022205e1680be7ee7975a91129cd8f
Willian Batista Viana <willian.batista.viana@gmail.com>
2016-09-07 16:39:19 -0300

    passar frases para um objeto e transformar os indices em array

--- back-end/project/src/index.js
+++ back-end/project/src/index.js
@@ -1,15 +1,29 @@
 var Crawler = require("js-crawler");
-var crawlerjs = require('crawler-js');
+// var crawlerjs = require('crawler-js');
 var cheerio = require('cheerio');
+
+
 var $;
+var verb; 
+var objectPhrase = {};
+
 
 new Crawler().configure({depth: 2})
   .crawl("https://www.englishclub.com/ref/Phrasal_Verbs", function onSuccess(page) {
     // console.log(page.url);
     $ = cheerio.load(page.content.toString('utf-8'));
-    console.log($('.linklisting h3.linktitle a').text());
+    // console.log($('.linklisting h3.linktitle a').text());
+
+    verb = $('.linklisting h3.linktitle a').text();
+
+    if(!(verb in objectPhrase) && verb != '' && verb != undefined) {
+      objectPhrase[verb] = true;
+    }
 
   });
+  
+  console.log(Object.keys(objectPhrase));
+
 
 // var phrasals = {
 //   interval: 1000,
@@ -39,6 +53,3 @@ new Crawler().configure({depth: 2})
 // }
  
 // crawlerjs(phrasals);
-
-
- 
