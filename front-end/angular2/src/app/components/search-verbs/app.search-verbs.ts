2e9d02a34a83783a3567f0bbeca018167c07a2f1
Willian Viana <willian.batista.viana@gmail.com>
2016-12-28 16:45:24 -0200

    Search verbs component added and running

--- front-end/angular2/src/app/components/search-verbs/app.search-verbs.ts
+++ front-end/angular2/src/app/components/search-verbs/app.search-verbs.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../services/get-verbs.service';
 
 @Component({
@@ -6,16 +6,26 @@ import { HttpService } from '../../services/get-verbs.service';
     templateUrl: './app.search-verbs.html',
 })
 
-export class searchVerbs{
+export class searchVerbs implements OnInit {
     getData : string;
     verbsBox : string;
+    verbs : Array<Object>;
     
     constructor(private _http : HttpService){}
+    
+    ngOnInit(){
+        this._http.getVerbsHTTP()
+        .then(data => {
+            this.verbs = JSON.parse(data._body); 
+            console.log('XPTO 2', data)
+            console.log('LET VERBS:', this.verbs)})
+        .catch(err => console.log(err))    
+    }
 
     getVerbs(){
         this._http.getVerbsHTTP()
         .then(data => {
-            this.getData = data._body; 
+            this.verbs = data._body; 
             console.log('XPTO 2', data)})
         .catch(err => console.log(err))    
     }
